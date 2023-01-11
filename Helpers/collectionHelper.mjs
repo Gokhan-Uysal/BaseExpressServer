224ad3246cf07a2834d0c0b5e6bdd3b004486b65
Gökhan Uysal <guysal20@ku.edu.tr>
2023-01-11 16:16:49 +0300

    New db crud operations added with many queries

--- Helpers/collectionHelper.mjs
+++ Helpers/collectionHelper.mjs
@@ -12,9 +12,23 @@ export class CollectionHelper{
         return insertId
     }
 
+    async insertManyItems(items){
+        if (items.length == 1){
+            let inserId = await this.insertOneItem(items[0])
+            return inserId
+        }
+        let insertIds = await this.getCollection().insertMany(items)
+        return insertIds
+    }
+
     async findOneItem(query){
         let foundItem = await this.getCollection().findOne(query)
         return foundItem
+    }
+
+    async findManyItems(query, limit){
+        let foundItems = await this.getCollection().find(query).limit(limit).toArray()
+        return foundItems
     }
 
     async updateOneItem(query , newQuery){
@@ -22,8 +36,18 @@ export class CollectionHelper{
         return updateResp
     }
 
+    async updateManyItems(query, newQuery){
+        let updateResp = await this.getCollection().updateMany(query, newQuery)
+        return updateResp
+    }
+
     async deleteOneItem(query){
         let deleteResp = await this.getCollection().deleteOne(query)
+        return deleteResp
+    }
+
+    async deleteManyItems(query){
+        let deleteResp = await this.getCollection().deleteMany(query)
         return deleteResp
     }
     
